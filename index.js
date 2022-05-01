0ed8699138dcf561ebc4578ac0c7b04da5f8af0d
GeunWoo <gktmdahr99@naver.com>
2022-05-01 16:24:21 +0900

    feat: 가장 간단한 페이지 생성

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-console.log(`1 더하기 2는 ${add(1, 2)}`);
+const express = require('express');
 
-console.log('1 더하기 2는 ' + add(1, 2));
+const app = express();
 
-function add(left, right) {
-    return left + right;
-}
+app.get('/', (req, res) => {
+    console.log(`New request! body: ${req.body}, query: ${JSON.stringify(req.query)}`);
+
+    res.send(`
+        <html lang="kr">
+            <head>
+                <title>Coding Party</title>
+            </head>
+            <body>
+                Welcome to the <b>Coding Party</b>
+            </body>
+        </html>
+    `);
+});
+
+const port = 3000;
+
+app.listen(port);
+
+console.log(`Server started listening on port ${port}!`);
