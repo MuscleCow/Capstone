8f16e5cae00f41db025fcbbf4155935d7c9d5925
GeunWoo <gktmdahr99@naver.com>
2022-05-10 22:31:29 +0900

    feat: 빙빙 돌아가는 회전목마처럼 Web이 돌아감

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+
+/**********************************************************************************
+ * IMPORTS
+ **********************************************************************************/
 const express = require('express');
+const Sensor = require('./Sensor');
+const SampleRepository = require('./SampleRepository');
+
+
+/**********************************************************************************
+ * READ FROM SENSOR EVERY 1 SEC
+ **********************************************************************************/
+
+setInterval(async () => {
+    const rawValue = Sensor.read();
+    const value = rawValue;
+
+    await SampleRepository.createNewSample(value, rawValue);
+}, 1000);
+
+
+/**********************************************************************************
+ * SERVE ON WEB
+ **********************************************************************************/
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     console.log(`New request! body: ${req.body}, query: ${JSON.stringify(req.query)}`);
+
+    const allSamples = await SampleRepository.getAllSamples();
 
     res.send(`
         <html lang="kr">
@@ -12,6 +37,10 @@ app.get('/', (req, res) => {
             </head>
             <body>
                 Welcome to the <b>Coding Party</b>
+                
+                <ul>
+                    ${allSamples.map(sample => `<li>${JSON.stringify(sample)}</li>`).join()}
+                </ul>
             </body>
         </html>
     `);
